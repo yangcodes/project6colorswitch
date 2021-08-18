54a237fb47d668cc0d194fae4a343f474e97e553
Jenny Lin <misslinyang91@gmail.com>
2021-08-18 11:09:51 +1000

    added color string text and background colore change effect

---
+++ app.js
@@ -0,0 +1,18 @@
+const body = document.body;
+const switchBtn = document.querySelector(".switch");
+const switchPara = document.querySelector(".color");
+
+switchBtn.addEventListener("click", function () {
+  let color1 = getRandomNum(); //red
+  let color2 = getRandomNum(); //green
+  let color3 = getRandomNum(); //blue
+
+  const colorString = `rgb(${color1}, ${color2}, ${color3})`;
+
+  body.style.backgroundColor = colorString;
+  switchPara.innerText = colorString;
+});
+
+function getRandomNum() {
+  return Math.floor(Math.random() * 255);
+}
